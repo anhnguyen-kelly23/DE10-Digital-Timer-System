78504e729b9d4c85009acd9e6335fc6c6aab015a
anhnguyen-kelly23 <anhnguyen-kelly23@users.noreply.github.com>
2026-04-20 14:13:45 +0700

    delete unuse file

--- Software/Bai4/obj/default/runtime/sim/mentor/load_sim.tcl
+++
@@ -1,8 +0,0 @@
-# ------------------------------------------------------------------------------
-# Top Level Simulation Script to source msim_setup.tcl
-# ------------------------------------------------------------------------------
-set QSYS_SIMDIR obj/default/runtime/sim
-source msim_setup.tcl
-# Copy generated memory initialization hex and dat file(s) to current directory
-file copy -force D:/Quartus/Bai4/Software/Bai4/mem_init/hdl_sim/system_onchip_memory2_0.dat ./ 
-file copy -force D:/Quartus/Bai4/Software/Bai4/mem_init/system_onchip_memory2_0.hex ./ 
